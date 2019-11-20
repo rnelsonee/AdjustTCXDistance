d633d0885b29154b0b1020b4d220239bf21567a4
Rick Nelson <rick.nelson@jhuapl.edu>
2019-11-20 14:53:16 -0500

    Add feature to allow for km instead of miles

--- AdjustMiles.py
+++ AdjustMiles.py
@@ -8,7 +8,7 @@
 # The program then asks you for your new distance in miles, and then adjusts that tag_open, and all other
 # DistanceMeters tag_opens, by that correction factor.
 #
-# The output is a new file (originalfile_corrected.txt, same folder) that has all new "stretched" distances
+# The output is a new file (originalfile_corrected.txt, same folder) that has all new "tag_valueetched" distances
 #
 # Version history
 # Ver   User    Description
@@ -19,48 +19,63 @@
 from tkinter import filedialog
 from tkinter import *
 
+def miles_or_not(question):
+    reply = input(question)
+    if reply[0].lower() == "y":
+        return True
+    elif reply[0].lower() == "n":
+        return False
+    else:
+        print("Invalid answer, please answer with a \"y\" or an \"n\"")
+        miles_or_not(question)
+
+
 # Ask for TCX file, get path
 root = Tk()
 root.filename =  filedialog.askopenfilename(initialdir = ".",title = "Select file",filetypes = [("TCX files","*.tcx")])
 
 # Set filename to write to. It will take abc.tcx and write to abc_correctd.tcx
-new_file_path = str(root.filename)
+new_file_path = root.filename
 new_file_path = new_file_path[:new_file_path.rfind(".")] + '_corrected.' + new_file_path[new_file_path.rfind(".")+1:]
 
 # Load files
 file_ori = open(root.filename,"r")
 file_dst = open(new_file_path, "w")
 
-meters_found = False
 text_marker = "DistanceMeters"
 tag_open = "<" + text_marker + ">"
 tag_close = "</" + text_marker + ">"
 lines_replaced = 0
+use_miles = True
 
 print("")
 for line in file_ori:
     # Three conditions: Finding DistanceMeters first time, finding subsequent times, and else
     # Set correction factor in first one, write new line. Write new (corrected) line in second case, write line as is in else
-    str = line[line.find("<"):line.find(">")+1]
+    tag_value = line[line.find("<"):line.find(">")+1]
 
-    if (str == tag_open) and (meters_found == False):
+    if (tag_value == tag_open) and (lines_replaced == 0):
 
         meters_found = True
         
         # Get number between "...>" and "</...."
         meters_orig = float(line[line.find(">")+1:line.find('</')])               # Grab the distance in meters
-        miles_orig = meters_orig/1609.34                        # Convert to miles
-        print("Original file states you ran %0.2f miles" % miles_orig)
-        
-        miles_new = float(input("What is the corrected mileage? "))
-        correction_factor = miles_new/miles_orig
-        print("The correction factor is %.3f/%.3f = %.3f" % (miles_new, miles_orig, correction_factor))
+
+        use_miles = miles_or_not("Would you like to use miles? (y = miles, n = km): ")
+        divisor = (1609.34 if use_miles else 1000)
+
+        distance_orig = meters_orig/divisor
+        print("Original file states you ran %0.2f " % distance_orig + ("miles" if use_miles else "km"))
+
+        distance_new = float(input("What is the corrected distance (" + ("miles" if use_miles else "km") +")? "))
+        correction_factor = distance_new/distance_orig
+        print("The correction factor is %.3f/%.3f = %.3f" % (distance_new, distance_orig, correction_factor))
         
         new_text = line[:line.find('>')] + '>' + f'{meters_orig*correction_factor:.5f}' + tag_close
         file_dst.write(new_text+'\n')
         lines_replaced += 1
     
-    elif (str == tag_open) and (meters_found == True):
+    elif (tag_value == tag_open) and (lines_replaced > 0):
         # This is for all the other times we find "<DistanceMeters>"
         # We just want to replace the number and write that new line out.
 
@@ -80,3 +95,4 @@ file_ori.close()
 file_dst.close()
 print("\n%d lines replaced." % lines_replaced)
 print("New file is " + new_file_path)
+
